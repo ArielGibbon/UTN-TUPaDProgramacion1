d4e78c4eb5d49587f6218f93503c2f260ff6e6a7
ArielGibbon <gibboncharo@gmail.com>
2025-10-30 19:15:06 -0300

    Ejercicios 9 y 10 terminados y probados

--- UNIDAD7/TP_7_DATOS_COMPLEJOS.py
+++ UNIDAD7/TP_7_DATOS_COMPLEJOS.py
@@ -1,101 +1,101 @@
-# # Dado el diccionario precios_frutas 
-# #precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450} 
-# #Añadir las siguientes frutas con sus respectivos precios: 
-# #● Naranja = 1200 
-# #● Manzana = 1500 
-# #● Pera = 2300 
+# Dado el diccionario precios_frutas 
+#precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 1450} 
+#Añadir las siguientes frutas con sus respectivos precios: 
+#● Naranja = 1200 
+#● Manzana = 1500 
+#● Pera = 2300 
 
-# precios_frutas = {"Banana": "1200", "Anana": "2500", "Melon": "3000", "Uva": "1450"}
-# #Añadir
-# precios_frutas["Naranja"] = "1200"
-# precios_frutas["Manzana"] = "1500"
-# precios_frutas["Pera"] = "2300"
+precios_frutas = {"Banana": "1200", "Anana": "2500", "Melon": "3000", "Uva": "1450"}
+#Añadir
+precios_frutas["Naranja"] = "1200"
+precios_frutas["Manzana"] = "1500"
+precios_frutas["Pera"] = "2300"
 
-################################################################################################################
+###############################################################################################################
 
-# # Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
-# #desarrollado en el punto anterior, actualizar los precios de las siguientes frutas: 
-# #● Banana = 1330 
-# #● Manzana = 1700 
-# #● Melón = 2800
+# Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
+#desarrollado en el punto anterior, actualizar los precios de las siguientes frutas: 
+#● Banana = 1330 
+#● Manzana = 1700 
+#● Melón = 2800
 
-# #Actualizar precios
-# precios_frutas["Banana"] = '1330'
-# precios_frutas["Manzana"] = '1700'
-# precios_frutas["Melon"] = '2800'
-# print (precios_frutas)
+#Actualizar precios
+precios_frutas["Banana"] = '1330'
+precios_frutas["Manzana"] = '1700'
+precios_frutas["Melon"] = '2800'
+print (precios_frutas)
+
+#########################################################################################################
+
+#3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
+#desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los 
+#precios.
+
+lista_frutas = list(precios_frutas.keys())
+print (lista_frutas)
 
 ##########################################################################################################
-
-# #3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
-# #desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los 
-# #precios.
-
-# lista_frutas = list(precios_frutas.keys())
-# print (lista_frutas)
-
-###########################################################################################################
 
 #4) Escribí un programa que permita almacenar y consultar números telefónicos. 
 #• Permití al usuario cargar 5 contactos con su nombre como clave y número como valor. 
 #• Luego, pedí un nombre y mostrale el número asociado, si existe.
 
-# agenda_telefonos = {}
-# for i in range (3):
-#     agenda_telefonos[input ("Ingrese el nombre de contacto ")] = input ("Ingrese el número del contacto ")
+agenda_telefonos = {}
+for i in range (3):
+    agenda_telefonos[input ("Ingrese el nombre de contacto ")] = input ("Ingrese el número del contacto ")
 
-# print(agenda_telefonos[input("Consultar: ")])
+print(agenda_telefonos[input("Consultar: ")])
 
-#print (agenda_telefonos)
+print (agenda_telefonos)
 
-########################################################################################################
+#######################################################################################################
 
 #5) Solicita al usuario una frase e imprime: 
 #• Las palabras únicas (usando un set). 
 #• Un diccionario con la cantidad de veces que aparece cada palabra.
 
-# palabras_set = ()
-# cantidad_diccionario = {}
+palabras_set = ()
+cantidad_diccionario = {}
 
-# frase = (input("Ingrese una frase "))
-# palabras = frase.split()
-# palabras_set = set(frase.split())
+frase = (input("Ingrese una frase "))
+palabras = frase.split()
+palabras_set = set(frase.split())
 
-# for palabra in palabras:
-#     if palabra in cantidad_diccionario:
-#         cantidad_diccionario[palabra] += 1
-#     else:
-#         cantidad_diccionario[palabra] = 1
+for palabra in palabras:
+    if palabra in cantidad_diccionario:
+        cantidad_diccionario[palabra] += 1
+    else:
+        cantidad_diccionario[palabra] = 1
 
-# print (frase)
-# print (palabras_set)
-# print (cantidad_diccionario)
+print (frase)
+print (palabras_set)
+print (cantidad_diccionario)
 
-###############################################################################################################
+##############################################################################################################
 
 #6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas. 
-# Luego, mostrá el promedio de cada alumno. 
+#Luego, mostrá el promedio de cada alumno. 
 
-# alumnos = {}   #diccionario
+alumnos = {}   #diccionario
 
-# for i in range(3):
-#     nombre = input(f"Ingrese el nombre del alumno {i + 1} ")  #nombre de alumnos
+for i in range(3):
+    nombre = input(f"Ingrese el nombre del alumno {i + 1} ")  #nombre de alumnos
 
-#     nota_1 = float(input(f"Ingrese la primera nota de {nombre}: "))     #notas de alumnos
-#     nota_2 = float(input(f"Ingrese la segunda nota de {nombre}: "))
-#     nota_3 = float(input(f"Ingrese la tercera nota de {nombre}: "))
+    nota_1 = float(input(f"Ingrese la primera nota de {nombre}: "))     #notas de alumnos
+    nota_2 = float(input(f"Ingrese la segunda nota de {nombre}: "))
+    nota_3 = float(input(f"Ingrese la tercera nota de {nombre}: "))
 
-#     notas = (nota_1, nota_2, nota_3) #tupla
-#     alumnos[nombre] = notas
+    notas = (nota_1, nota_2, nota_3) #tupla
+    alumnos[nombre] = notas
 
-# print("\n=== PROMEDIOS ===")        #promedios
-# for nombre, notas in alumnos.items():
-#     promedio = sum(notas) / len(notas)
-#     print(f"{nombre}: {promedio:.2f}")
+print("\n=== PROMEDIOS ===")        #promedios
+for nombre, notas in alumnos.items():
+    promedio = sum(notas) / len(notas)
+    print(f"{nombre}: {promedio:.2f}")
 
-# print (alumnos)
+print (alumnos)
 
-#########################################################################################################
+########################################################################################################
 
 #7) Dado dos sets de números, representando dos listas de estudiantes que aprobaron Parcial 1 
 #y Parcial 2: 
@@ -103,16 +103,16 @@
 #• Mostrá los que aprobaron solo uno de los dos. 
 #• Mostrá la lista total de estudiantes que aprobaron al menos un parcial (sin repetir).
 
-# orden_de_alumnos_aprobados_parcial1 = {2,3,4,7,8,9}
-# orden_de_alumnos_aprobados_parcial2 = {1,2,4,7,9,10,11}
-# ambos_parciales = orden_de_alumnos_aprobados_parcial1 & orden_de_alumnos_aprobados_parcial2
-# un_solo_parcial = orden_de_alumnos_aprobados_parcial2 ^ orden_de_alumnos_aprobados_parcial1
-# al_menso_uno = orden_de_alumnos_aprobados_parcial1 | orden_de_alumnos_aprobados_parcial2
-# print (f"Los alumnos que aprobaron ambos parciales son: {ambos_parciales} ")
-# print (f"Los alumnos que aprobaron un solo parcial son: {un_solo_parcial} ")
-# print (f"Los alumnos que aprobaron al menos un parcial son: {al_menso_uno}")
+orden_de_alumnos_aprobados_parcial1 = {2,3,4,7,8,9}
+orden_de_alumnos_aprobados_parcial2 = {1,2,4,7,9,10,11}
+ambos_parciales = orden_de_alumnos_aprobados_parcial1 & orden_de_alumnos_aprobados_parcial2
+un_solo_parcial = orden_de_alumnos_aprobados_parcial2 ^ orden_de_alumnos_aprobados_parcial1
+al_menso_uno = orden_de_alumnos_aprobados_parcial1 | orden_de_alumnos_aprobados_parcial2
+print (f"Los alumnos que aprobaron ambos parciales son: {ambos_parciales} ")
+print (f"Los alumnos que aprobaron un solo parcial son: {un_solo_parcial} ")
+print (f"Los alumnos que aprobaron al menos un parcial son: {al_menso_uno}")
 
-############################################################################################################
+###########################################################################################################
 
 #8) Armá un diccionario donde las claves sean nombres de productos y los valores su stock. 
 #Permití al usuario: 
@@ -157,13 +157,71 @@ while True:         #opciones
     elif opcion == "3":             
         break
 
-##################################################################################################################
+#################################################################################################################
 
 #9) Creá una agenda donde las claves sean tuplas de (día, hora) y los valores sean eventos.
 #Permití consultar qué actividad hay en cierto día y hora.
 
-# Agenda con tuplas (día, hora) como claves
-#agenda = (lunes)
+#Se crea el diccionario usando las tuplas de key(dia y hora) y los eventos de value
+agenda = {
+    ("lunes", "10:00"): "Clases de la Facu",
+    ("martes", "10:00"): "Reunión de trabajo",
+    ("miercoles", "10:00"): "Gym",
+    ("jueves", "22:00"): "Fútbol con los pibes",
+    ("viernes", "22:00"): "Cena de amigos"
+}
+##un bucle while para hacer el menu
+while True:
+    print("\n1. Consultar actividad")
+    print("2. Agregar actividad")
+    print("3. Salir")
+    
+    opcion = input("Elegí una opción: ")
+    
+    if opcion == "1":
+        dia = input("Ingresá el día: (en minúsculas)")
+        hora = input("Ingresá la hora: (en formato hh:mm)")
+        
+        if (dia, hora) in agenda:
+            print(f"Actividad: {agenda[(dia, hora)]}")
+        else:
+            print("No hay actividad programada")
+    
+    elif opcion == "2":
+        dia = input("Ingresá el día: ")
+        hora = input("Ingresá la hora: ")
+        evento = input("Ingresá el evento: ")
+        
+        agenda[(dia, hora)] = evento
+        print("Actividad agregada")
+    
+    elif opcion == "3":
+        break
+
+##################################################################################################
+
+#10) Dado un diccionario que mapea nombres de países con sus capitales, construí un nuevo
+#diccionario donde:
+#• Las capitales sean las claves.
+#• Los países sean los valores.
+
+paises_capitales = {
+    "Argentina": "Buenos Aires",
+    "Brasil": "Brasilia",
+    "Chile": "Santiago",
+    "Uruguay": "Montevideo"
+}
+
+capitales_paises = {}
+
+for pais, capital in paises_capitales.items():
+    capitales_paises[capital] = pais
+
+print(capitales_paises)
+
+
+
+
 
 
 
